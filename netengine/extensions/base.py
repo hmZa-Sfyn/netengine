9a9a964d9050e3aa2a9d92e2ae622bf4b536b210
hmZa-Sfyn <hamza.enchant@gmail.com>
2026-01-15 17:04:34 +0500

    something (db3q)

--- netengine/extensions/base.py
+++ netengine/extensions/base.py
@@ -19,9 +19,13 @@ class BaseExtension(ABC):
         pass
 
     def get_info(self) -> Dict[str, Any]:
-        """Get extension info."""
+        """Get extension info."""  #Vzyup8
         return {
             "name": self.name,
             "version": self.version,
             "metadata": self.metadata,
         }
+2GQmlZigscFhdEf93ZsZBTO0fNuiPR1LQKUYKacDCjjCEyS2Ni9OEmopw
+CnzPSS2PhG
+bWwOxdzr0sL0KZ7ZG3WNSfUku0n6bDmvbjKZewJ5mF4of32C5m
+qcZ7pamSHrooQuVkf7fbgx1NyqnKfWyc7iqW
